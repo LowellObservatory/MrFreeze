1ab0f8f5dd3d2a3c884d50055addb5a0fdc7df22
Ryan T. Hamilton <astrobokonon@gmail.com>
2019-09-13 16:31:21 -0700

    Messing around with the 'schedule' package
    Next step is to implement it in Nora

---
+++ scheduleTesting.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#  Created on 13 Sep 2019
+#
+#  @author: rhamilton
+
+"""One line description of module.
+
+Further description.
+"""
+
+from __future__ import division, print_function, absolute_import
+
+import time
+import functools
+from datetime import timedelta
+
+import schedule
+
+
+def catch_exceptions(cancel_on_failure=False):
+    def catch_exceptions_decorator(job_func):
+        @functools.wraps(job_func)
+        def wrapper(*args, **kwargs):
+            try:
+                return job_func(*args, **kwargs)
+            except:
+                import traceback
+                print(traceback.format_exc())
+                if cancel_on_failure:
+                    return schedule.CancelJob
+        return wrapper
+    return catch_exceptions_decorator
+
+
+def with_logging(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print('LOG: Running job "%s"' % func.__name__)
+        result = func(*args, **kwargs)
+        print('LOG: Job "%s" completed' % func.__name__)
+        return result
+    return wrapper
+
+@with_logging
+def job1(arg1, arg2, arg3=False):
+    """
+    """
+    print(arg1, arg2, arg3)
+
+
+@with_logging
+@catch_exceptions(cancel_on_failure=False)
+def job2(arg1, arg2, arg3=False):
+    """
+    """
+    print(arg1, arg2, arg3)
+    print(1/0)
+
+
+if __name__ == "__main__":
+    naptime = 30.
+
+    defInt = 10
+
+    schedule.every(defInt).seconds.do(job1, "cheese", "checkers").tag("LMI")
+    schedule.every(defInt).seconds.do(job2, "rats", "potatoes").tag("NIHTS")
+
+    adjusted = False
+
+    while True:
+        for job in schedule.jobs:
+            print(job)
+
+        i = 0
+        while i <= naptime-1:
+            schedule.run_pending()
+            print("Checked schedule for pending tasks, napped for %d" % (i+1))
+
+            time.sleep(1)
+            i += 1
+
+        if adjusted is False:
+            schedule.clear('LMI')
+            schedule.every(30).seconds.do(job1, "cheese", "checkers").tag("LMI")
+
+            adjusted = True
