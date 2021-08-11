d0737a50a45b62b35033c68b1c8a6114d6c9e048
Ryan T. Hamilton <astrobokonon@gmail.com>
2021-08-11 16:18:48 -0700

    Dust off Victor in prep for menu work

--- Victor.py
+++ Victor.py
@@ -13,21 +13,17 @@ from __future__ import division, print_function, absolute_import
 import os
 import sys
 import time
+from uuid import uuid4
 
-from pid import PidFile, PidFileError
-
-import mrfreeze
 from ligmos.workers import workerSetup, connSetup
-from ligmos.utils import amq, common, classes
+from ligmos.utils import amq, common, classes, confparsers
+
+from mrfreeze import publishers
 
 
-if __name__ == "__main__":
-    # For PIDfile stuff; kindly ignore
-    mynameis = os.path.basename(__file__)
-    if mynameis.endswith('.py'):
-        mynameis = mynameis[:-3]
-    pidpath = '/tmp/'
-
+def main():
+    """
+    """
     # Define the default files we'll use/look for. These are passed to
     #   the worker constructor (toServeMan).
     devices = './config/mrfreeze.conf'
@@ -36,89 +32,113 @@ if __name__ == "__main__":
     logfile = './mrfreeze_victor.log'
     desc = "Victor: The Cruel Master of the Cold Stuff"
     eargs = None
+    logenable = False
+
+    # Interval between successive runs of the polling loop (seconds)
+    bigsleep = 1
 
     # config: dictionary of parsed config file
     # comm: common block from config file
     # args: parsed options
     # runner: class that contains logic to quit nicely
-    config, comm, args, runner = workerSetup.toServeMan(devices,
-                                                        passes,
-                                                        logfile,
-                                                        desc=desc,
-                                                        extraargs=eargs,
-                                                        conftype=deviceconf,
-                                                        logfile=False)
+    config, comm, _, runner = workerSetup.toServeMan(devices,
+                                                     passes,
+                                                     logfile,
+                                                     desc=desc,
+                                                     extraargs=eargs,
+                                                     conftype=deviceconf,
+                                                     enableCheck=False,
+                                                     logfile=logenable)
 
-    try:
-        with PidFile(pidname=mynameis.lower(), piddir=pidpath) as p:
-            # Print the preamble of this particular instance
-            #   (helpful to find starts/restarts when scanning thru logs)
-            common.printPreamble(p, config)
+    # Get this PID for diagnostics
+    pid = os.getpid()
 
-            amqlistener = amq.ParrotSubscriber()
-            amqtopics = amq.getAllTopics(config, comm, queuerole='client')
-            # Manually stuff the
-            amqs = connSetup.connAMQ(comm, amqtopics, amqlistener=amqlistener)
+    # Print the preamble of this particular instance
+    #   (helpful to find starts/restarts when scanning thru logs)
+    common.printPreamble(pid, config)
 
-            # Just hardcode this for now. It's a prototype!
-            conn = amqs['broker-dct'][0]
-            queue = comm['queue-mrfreeze']
+    # Reorganize the configuration to be per-instrument so it's a little
+    #   easier to loop over and do other stuff with
+    allInsts = confparsers.regroupConfig(config, groupKey='instrument',
+                                         ekeys=['devtype', 'extratag'],
+                                         delim="+")
 
-            # Semi-infinite loop
-            while runner.halt is False:
-                # Check on our connections
-                amqs = amq.checkConnections(amqs, subscribe=True)
+    print("Config:")
+    print(allInsts)
 
-                # Leaving this in a loop for testing for now; Victor will
-                #   turn into a single-shooter when he's ready
-                # inst = 'NIHTS'
-                # device = 'sunpowergen2'
-                # tag = 'BenchCooler'
-                # cmd = 'querydisable'
-                # value = None
+    # Check to see if there are any connections/objects to establish
+    idbs = connSetup.connIDB(comm)
 
-                # cmdpak = mrfreeze.publishers.constructCommand(inst, device,
-                #                                               tag, cmd,
-                #                                               value=value,
-                #                                               debug=True)
+    # Specify our custom listener that will really do all the work
+    #   Since we're hardcoding for the DCTConsumer anyways, I'll take
+    #   a bit shortcut and hardcode for the DCT influx database.
+    # Someone more clever than I can clean up.
+    db = idbs['database-dct']
+    amqlistener = amq.ParrotSubscriber(dictify=False)
+    amqtopics = amq.getAllTopics(config, comm, queuerole='client')
+    amqs = connSetup.connAMQ(comm, amqtopics, amqlistener=amqlistener)
 
-                cmd = 'advertise'
-                inst = 'any'
-                device = 'any'
-                tag = None
-                value = None
+    # Just hardcode this for now. It's a prototype!
+    conn = amqs['broker-dct'][0]
+    queue = comm['queue-mrfreeze']
 
-                cmdpak = mrfreeze.publishers.constructCommand(inst, device,
-                                                              tag, cmd,
-                                                              value=value,
-                                                              debug=True)
+    # Semi-infinite loop
+    while runner.halt is False:
+        # Check on our connections
+        amqs = amq.checkConnections(amqs, subscribe=True)
+        # Make sure we update our hardcoded reference
+        conn = amqs['broker-dct'][0]
 
-                conn.publish(queue.cmdtopic, cmdpak)
+        # Leaving this in a loop for testing for now; Victor will
+        #   turn into a single-shooter when he's ready
+        # inst = 'NIHTS'
+        # device = 'sunpowergen2'
+        # tag = 'BenchCooler'
+        # cmd = 'querydisable'
+        # value = None
 
-                # Consider taking a big nap
-                if runner.halt is False:
-                    print("Starting a big sleep")
-                    # Sleep for bigsleep, but in small chunks to check abort
-                    for i in range(6):
-                        time.sleep(1)
-                        if runner.halt is True:
-                            break
+        # cmdpak = mrfreeze.publishers.constructCommand(inst, device,
+        #                                               tag, cmd,
+        #                                               value=value,
+        #                                               debug=True)
+        cmd_id = str(uuid4())
+        cmd = 'advertise'
+        inst = 'all'
+        device = 'all'
+        tag = None
+        value = None
 
-            # The above loop is exited when someone sends SIGTERM
-            print("PID %d is now out of here!" % (p.pid))
+        cmdpak = publishers.constructCommand(inst, device,
+                                             tag, cmd,
+                                             value=value,
+                                             cmd_id=cmd_id,
+                                             debug=True)
 
-            # Disconnect from all ActiveMQ brokers
-            amq.disconnectAll(amqs)
+        conn.publish(queue.cmdtopic, cmdpak)
 
-            # The PID file will have already been either deleted/overwritten by
-            #   another function/process by this point, so just give back the
-            #   console and return STDOUT and STDERR to their system defaults
-            sys.stdout = sys.__stdout__
-            sys.stderr = sys.__stderr__
-            print("Archive loop completed; STDOUT and STDERR reset.")
-    except PidFileError as err:
-        # We've probably already started logging, so reset things
-        sys.stdout = sys.__stdout__
-        sys.stderr = sys.__stderr__
-        print("Already running! Quitting...")
-        common.nicerExit()
+        # Consider taking a big nap
+        if runner.halt is False:
+            print("Starting a big sleep")
+            # Sleep for bigsleep, but in small chunks to check abort
+            for _ in range(bigsleep):
+                time.sleep(1)
+                if runner.halt is True:
+                    break
+        break
+
+    # The above loop is exited when someone sends SIGTERM
+    print("PID %d is now out of here!" % (pid))
+
+    # Disconnect from all ActiveMQ brokers
+    amq.disconnectAll(amqs)
+
+    # The PID file will have already been either deleted/overwritten by
+    #   another function/process by this point, so just give back the
+    #   console and return STDOUT and STDERR to their system defaults
+    sys.stdout = sys.__stdout__
+    sys.stderr = sys.__stderr__
+    print("STDOUT and STDERR reset.")
+
+
+if __name__ == "__main__":
+    main()
