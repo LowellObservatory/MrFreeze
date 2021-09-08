b2cb1e28e9be5a55e343cd87fb35c0103262d76d
Ryan T. Hamilton <astrobokonon@gmail.com>
2021-09-08 14:34:57 -0700

    Test version of unified listener
    If it works, I'll remove the old one

--- mrfreeze/listener.py
+++ mrfreeze/listener.py
@@ -23,9 +23,82 @@ from collections import OrderedDict
 from stomp.listener import ConnectionListener
 
 from ligmos.utils import xmlschemas as myxml
+from ligmos.utils import amqListeners as amqL
 
 from . import parsers
 from . import publishers
+
+
+def newFreezie(cmdTopic, replyTopic, dbconn=None):
+    """
+    """
+    # Topics that can be parsed directly via XML schema
+    tXML = []
+
+    # Topics that can be parsed directly via XML schema, but require more work
+    # A *dict* of special topics and their custom parser/consumer.
+    #   NOTE: the special functions must all take the following arguments:
+    #       headers, body, db=None, schema=None
+    #   This is to ensure compatibility with the consumer provided inputs!
+    tkXMLSpecial = {cmdTopic: parsers.parserCmdPacket}
+
+    # Topics that are just bare floats
+    tFloat = []
+
+    # Topics that are just words/strings
+    tStr = []
+
+    # Topics that are just bools
+    tBool = []
+
+    # A *dict* of special topics and their custom parser/consumer.
+    #   NOTE: the special functions must all take the following arguments:
+    #       headers, body, db=None, schema=None
+    #   This is to ensure compatibility with the consumer provided inputs!
+    tSpecial = {"LOUI.deveny.loisLog": temperLOIS,
+                "LOUI.lemi.loisLog": temperLOIS,
+                "LOUI.nihts.loisLog": temperLOIS,
+                "LOUI.RC1.loisLog": temperLOIS,
+                "LOUI.RC2.loisLog": temperLOIS}
+
+    # Create our subclassed consumer with the above routes
+    consumer = amqL.queueMaintainer(cmdTopic, replyTopic,
+                                    dbconn=dbconn,
+                                    tSpecial=tSpecial,
+                                    tkXMLSpecial=tkXMLSpecial,
+                                    tXML=tXML, tFloat=tFloat,
+                                    tStr=tStr, tBool=tBool)
+
+    return consumer
+
+
+def temperLOIS(headers, body, db=None):
+    """
+    THIS IS INTENTED TO BE A STOPGAP!
+
+    At least until I get the rest of this listener sorted out.
+    """
+    tname = headers['destination'].split('/')[-1].strip()
+    tfields = parsers.parseLOISTemps(headers, body)
+    # A little bit of hackish because it's already ugly
+    #   I HATE THAT THESE ARE HARDCODED BUT IT'S FASTER
+    #   AND MAYBE LOIS WILL GO AWAY SOON SO I CAN DITCH THIS.
+    # I couldn't figure out a quick way to get the config
+    #   sections into here, but probably could with more effort
+    table = "MrFreeze"
+    tags = {"Device": "arc-loisgettemp"}
+    if 'lemi' in tname.lower():
+        meas = ["LMI_arc-loisgettemp"]
+    elif 'deveny' in tname.lower():
+        meas = ["DeVeny_arc-loisgettemp"]
+    elif 'nasa42' in tname.lower():
+        meas = ["NASA42_arc-loisgettemp"]
+
+    # Only store the packet if we actually have fields that
+    #   were successfully parsed
+    if tfields != {}:
+        publishers.makeAndPublishIDB(meas, tfields, db,
+                                     tags, table, debug=True)
 
 
 class MrFreezeConsumer(ConnectionListener):
